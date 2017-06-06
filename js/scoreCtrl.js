5093948842560c8eb001b910b3ad03930ea9f4c2
Frederic Huguenin <fred76temp@gmail.com>
2017-06-06 23:13:35 +0200

    display total score in round

--- js/scoreCtrl.js
+++ js/scoreCtrl.js
@@ -4,43 +4,18 @@ angular.module('app.controllers')
     var vm = this;
 
     vm.playerList = [];
-    vm.playerScores = [];
-    var score = {
-        point: null,
-        total: null
-    };
     vm.nbRound = [];
 
-
-
-    /******************************      FUNCTION DECLARATION             ************************/
+    /******************************      FUNCTION DECLARATION            ************************/
     vm.reset = reset;
     vm.addPlayer = addPlayer;
     vm.deletePlayer = deletePlayer;
     vm.addRound = addRound;
     vm.updateScore = updateScore;
+    vm.nbRound = [];
 
-    scoreInit = {
-        point: 0,
-        total: 0
-    };
-    vm.nbRound[0] = 0;
-
-    /******************************         INITIALISATION                ************************/
-    // create avatar list
-    var avatarList = ["avatar1.jpg",
-                    "avatar2.jpg",
-                    "avatar3.jpg",
-                    "avatar4.jpg",
-                    "avatar5.jpg",
-                    "avatar6.jpg",
-                    "avatar7.jpg",
-                    "avatar8.jpg",
-                    "avatar9.jpg",
-                    "avatar10.jpg",
-                    "avatar11.jpg"];
-
-
+    /******************************         INITIALISATION               ************************/
+    reset();
 
     /********************************************************************************************/
     /*                              PUBLIC FUNCTIONS DECLARATION
@@ -48,7 +23,7 @@ angular.module('app.controllers')
 
     /******************************         ADD NEW PLAYER               ************************/
     function addPlayer() {
-        var player = new playerFactory.Player("new player");
+        var player = new playerFactory.Player("player_" + vm.playerList.length);
         var randomIndex = Math.floor(Math.random() * (avatarList.length));
         player.avatar = avatarList[randomIndex];
         avatarList.splice(randomIndex, 1); // remove the selected avatar from list
@@ -59,14 +34,14 @@ angular.module('app.controllers')
         vm.playerList.push(player);
 
         // create empty playerScore for each round
-        for (var round = 0; round <= vm.nbRound.length; round++) {
+        for (var round = 0; round < vm.nbRound.length; round++) {
             vm.playerList[player.num].score[round] = {};
             vm.playerList[player.num].score[round].point = 0;
             vm.playerList[player.num].score[round].total = 0;
         }
     }
 
-    /******************************         DELETE PLAYER                ************************/
+    /******************************         DELETE PLAYER         *******************************/
     function deletePlayer(item, index) {
         // reintroduce avatar at the proper position
         var avatIndex = parseInt(item.avatar.slice(6)) - 1;
@@ -81,30 +56,33 @@ angular.module('app.controllers')
     }
 
 
-    /******************************         ADD ROUND                ************************/
+    /******************************         ADD ROUND                ***************************/
     function addRound() {
 
         // add an empty score for each player
         for (var playerNum = 0; playerNum < vm.playerList.length; playerNum++) {
             vm.playerList[playerNum].score[vm.nbRound.length] = {};
             vm.playerList[playerNum].score[vm.nbRound.length].point = 0;
-            vm.playerList[playerNum].score[vm.nbRound.length].total = 0;
+            vm.playerList[playerNum].score[vm.nbRound.length].total = vm.playerList[playerNum].score[vm.nbRound.length - 1].total;
         }
 
         vm.nbRound.push(vm.nbRound.length);
     }
 
 
-    /******************************         UPDATE SCORE                ************************/
+    /******************************         UPDATE SCORE             ***************************/
     function updateScore(round, playerNum) {
-        var coucou = "coucou";
+        for (var i = round; i < vm.nbRound.length; i++) {
+            vm.playerList[playerNum].score[i].total = parseFloat(vm.playerList[playerNum].score[i - 1].total) + parseFloat(vm.playerList[playerNum].score[i].point);
+        }
     }
 
 
-    /******************************         CLEAR ROUND                ************************/
+    /******************************         CLEAR ROUND                *************************/
     function reset() {
         vm.playerList = [];
         vm.nbRound = [];
+        vm.nbRound[0] = 0;
         avatarList = ["avatar1.jpg",
                     "avatar2.jpg",
                     "avatar3.jpg",
